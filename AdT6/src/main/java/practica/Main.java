7428594f049103633de17f165f5e0d82f6e1b591
joseanton <joseantonioramoschamorro@gmail.com>
2021-02-14 18:59:35 +0100

    segunda subida

--- AdT6/src/main/java/practica/Main.java
+++ AdT6/src/main/java/practica/Main.java
@@ -1,13 +1,17 @@
 package practica;
-import java.util.ArrayList;
-import java.util.List;
-import org.*;
+
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class Main {
 	public static void main(String[] args) {
-		ApplicationContext appContext = 
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("recursospractica/xml/beans_constructorargs.xml");
+		ApplicationContext appContext1 =	
 				new ClassPathXmlApplicationContext("recursospractica/xml/beans_constructorargs.xml");
-		
+		Centro direc = (Centro) appContext1.getBean("ingles");
+		System.out.println(direc.toString());
 		
 		
 	}
